d88437f5e790a28f52c52c58644d639297450bd4
Laudini <krzeminski.yt.kamil@gmail.com>
2020-09-27 13:30:40 +0200

    added Skeld tasks to !task command

--- src/app/bot.js
+++ src/app/bot.js
@@ -1,8 +1,19 @@
 require("dotenv").config();
+const tasks = require("../assets/tasks/tasks");
 
 const token = process.env.DISCORD_BOT_TOKEN;
 
 const { Client } = require("discord.js");
 const client = new Client();
 
+client.on("message", (message) => {
+  message.content === "!task" && 
+  message.reply(pickRandomObjectvalue(tasks));
+});
+
 client.login(process.env.DISCORD_BOT_TOKEN);
+
+const pickRandomObjectvalue = (data) => {
+  let keys = Object.keys(data);
+  return data[keys[(keys.length * Math.random()) << 0]].description;
+};
---
+++ src/assets/tasks/tasks.js
@@ -0,0 +1,85 @@
+const tasks = {
+    alignEngine : {
+        description: "Nakierowanie rakiety w engine",
+        map: "Skeld",
+        duration: "short",
+    },
+    calibrateDistributor : {
+        description: "Trzy przyciski w elektryku",
+        map: "Skeld",
+        duration: "short",
+    },
+    chartCourse : {
+        description: "Przesuwanie rakiety po punktach w navi",
+        map: "Skeld",
+        duration: "short"
+    },
+    oxygenfilter : {
+        description: "Liście w oxygenie",
+        map: "Skeld",
+        duration: "short"
+    },
+    clearAsteroids : {
+        description: "Strzelanie w asteroidy",
+        map: "Skeld",
+        duration: "long"
+    },
+    divertPower : {
+        description: "Suwak w elektryku",
+        map: "Skeld",
+        duration: "short/long"
+    },
+    emptyShut : {
+        description: "Wyrzucenie śmieci w storage/cafeterii",
+        map: "Skeld",
+        duration: "long"
+    },
+    fixWiring : {
+        description: "Kabelki :D (admin/cafeteria/elektryk/navi/security/storage",
+        map: "Skeld",
+        duration: "common"
+    },
+    refuelEngines : {
+        description: "Paliwko w górnym/dolnym silniku",
+        map: "Skeld",
+        duration: "long"
+    },
+    inspectSample : {
+        description: "Probówka w medbay",
+        map: "Skeld",
+        duration: "long"
+    },
+    primeShields : {
+        description: "Osłonki/shieldy",
+        map: "Skeld",
+        duration: "short"
+    },
+    stabilizeSteerings : {
+        description: "Ustawienie celownika sterowniczego w Navi",
+        map: "Skeld",
+        duration: "short"
+    },
+    startReactor : {
+        description: "Memory w reactorze / odpalenie reactora",
+        map: "Skeld",
+        duration: "long"
+    },
+    submitScan : {
+        description: "Scan w medbayu",
+        map: "Skeld",
+        duration: "long"
+    },
+    swipeCard : {
+        description: "Przesuwanie karty w adminie",
+        map: "Skeld",
+        duration: "common"
+    },
+    uploadData : {
+        description: "Zrobienie downloadu w Cafeteria/Comms/Elektryk/Navi/Weapons a potem uploadu do admina",
+        map: "Skeld",
+        duration: "long"
+    }
+
+}
+
+module.exports = tasks;
